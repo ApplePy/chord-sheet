e56aa69083026b156ed53302e71c1c9b5f74c6ce
Darryl Murray <darrylmurray@me.com>
2016-11-18 22:19:40 -0500

    Added semantic ui.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {SuiModule} from 'ng2-semantic-ui/ng2-semantic-ui';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    SuiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
