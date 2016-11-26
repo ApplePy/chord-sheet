a5a32c1d5c9a5210bef45212ce2c0c78ce3041a4
Darryl Murray <darrylmurray@me.com>
2016-11-26 06:59:48 -0500

    Some minor changes to work with the updated validator.

--- chord-sheet/src/app/components/edit-screen/edit-screen.component.ts
+++ chord-sheet/src/app/components/edit-screen/edit-screen.component.ts
@@ -99,39 +99,40 @@ export class EditScreenComponent implements OnInit {
     let results = this.validator.validate(contents);
 
     // Send if no errors, otherwise display errors
-    if (results.errors.length == 0 && results.warnings.length == 0) {
+    if (!results.containsIssues()) {
       // TODO: Send data
+      event.preventDefault();
     } else {
       // Set errors
-      this.error.setMessages("Parse Errors", results.errors);
-      this.warning.setMessages("Parse Warnings", results.warnings);
+      if (results.errors.length) this.error.setMessages("Parse Errors", results.errors);
+      if (results.warnings.length) this.warning.setMessages("Parse Warnings", results.warnings);
       event.preventDefault();
     }
   }
 }
 
-/*  testString:string = "{t:Hallelujah}\n\
- {st:Jeff Buckley}\n\
- {c: Sheet Music by Leonord Cohen}\n\
- \n\
- #\n\
- #This tab is for Hallelujah, as i've found i've not really gotten on with the other ones\n\
- #here. The song is obviously picked but if you find that too difficult it also works\n\
- #well strummed but picking the root notes of the chord with your thumb. With the G and E7\n\
- #the start of each verse do a 2-3 up to the G and a 2-0 on the way down to the E7 on the\n\
- #E string.\n\
- {sot}\n\
- \n\
- {eot}\n\
- [G]  [Em]  [G]  [Em]\n\
- \n\
- [G]I heard there was a s[Em]ecret chord\n\
- That [G]David played and it [Em]pleased the Lord\n\
- But [C]you don't really [D7] care for music, d[G]o ya?   [D7]\n\
- W[G]ell it goes like this the [C]fourth, the [D7]fifth\n\
- [Em]The minor fall and the [C]major lift\n\
- The [D7]baffled king [B7]composing halle[Em]lujah\n\
- \n\
- {soc}\n\
- Ha[C]llelujah, ha[Em]llelujah, ha[C]llelujah, ha[G]llelu[D7]-u-u-u-ja[G]aah     [Em]  [G]  [Em]\n\
- {eoc}";*/
+// var  testString:string = "{t:Hallelujah} \n\
+//  {st:Jeff Buckley}\n\
+//  {c: Sheet Music by Leonord Cohen}\n\
+//  \n\
+//  #\n\
+//  #This tab is for Hallelujah, as i've found i've not really gotten on with the other ones\n\
+//  #here. The song is obviously picked but if you find that too difficult it also works\n\
+//  #well strummed but picking the root notes of the chord with your thumb. With the G and E7\n\
+//  #the start of each verse do a 2-3 up to the G and a 2-0 on the way down to the E7 on the\n\
+//  #E string.\n\
+//  {sot}\n\
+//  \n\
+//  {eot}\n\
+//  [G]  [Em]  [G]  [Em]\n\
+//  \n\
+//  [G]I heard there was a s[Em]ecret chord\n\
+//  That [G]David played and it [Em]pleased the Lord\n\
+//  But [C]you don't really [D7] care for music, d[G]o ya?   [D7]\n\
+//  W[G]ell it goes like this the [C]fourth, the [D7]fifth\n\
+//  [Em]The minor fall and the [C]major lift\n\
+//  The [D7]baffled king [B7]composing halle[Em]lujah\n\
+//  \n\
+//  {soc}\n\
+//  Ha[C]llelujah, ha[Em]llelujah, ha[C]llelujah, ha[G]llelu[D7]-u-u-u-ja[G]aah     [Em]  [G]  [Em]\n\
+//  {eoc}";
