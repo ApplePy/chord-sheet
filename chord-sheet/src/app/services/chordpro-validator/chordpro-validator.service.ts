1ae3bd0d7b8f2009bf4f52ba4b05b5021d52f260
Darryl Murray <darrylmurray@me.com>
2016-11-26 06:57:28 -0500

    Finally got chordsheet validation working.

--- chord-sheet/src/app/services/chordpro-validator/chordpro-validator.service.ts
+++ chord-sheet/src/app/services/chordpro-validator/chordpro-validator.service.ts
@@ -1,230 +1,178 @@
 import { Injectable } from '@angular/core';
+import {Directive} from "./directive";
+import {ChordProValidatorOutput} from "./chordpro-validator-output";
+import {DirectiveDefinition} from "./directive-definition";
+import {isNullOrUndefined} from "util";
 
+/**
+ * This service verifies ChordPro data.
+ */
 @Injectable()
 export class ChordproValidatorService {
-// TODO: Document all of this
 
+  readonly extra_chars_func = function(input: Directive, definition: DirectiveDefinition, output: ChordProValidatorOutput): boolean {
+    // Check definition directives to see if they are substrings of the input directive.
+    for (let def of definition.directives) {
+
+      // This is a soc with extra characters
+      if (input.directive.indexOf(def) != -1) {
+        output.errors.push("Error: Extra characters for “soc”, “eoc”, “sot” or “eot” directives.");
+        return false;
+      }
+    }
+    return false;
+  };
+
+  acceptableDirectives: DirectiveDefinition[] =
+  [
+    new DirectiveDefinition(["new_song","ns"], false, null, true, ["new_song", "ns"]),
+    new DirectiveDefinition(["title",  "t"], /.*/, "Error: Missing separator (“:”) or text for “title”, “subtitle”, “comment” or “define” directives.", true, false, false,
+      // Stack Function
+      function(stack: Directive[], definition: DirectiveDefinition, output: ChordProValidatorOutput) {
+        let foundOnce = false;
+        for (let item of stack) {
+          // If a title directive has been found
+          if (definition.directives.indexOf(item.directive) == 0) {
+            // If a title hasn't been seen, mark that one has. If another title is seen after, flag error.
+            if (!foundOnce) {
+              foundOnce = true;
+            }else {
+              output.errors.push("Error: More than one “title” directive within the same “new_song” block.");
+              break;
+            }
+          }
+        }
+      }),
+    new DirectiveDefinition(["subtitle", "st"], /.*/, "Error: Missing separator (“:”) or text for “title”, “subtitle”, “comment” or “define” directives.", false, false),
+    new DirectiveDefinition(["comment", "c"], /.*/, "Error: Missing separator (“:”) or text for “title”, “subtitle”, “comment” or “define” directives.", false, false),
+    new DirectiveDefinition(["start_of_chorus", "soc"], false, null, true, false, false, null, null, null, null,
+      // Directive No Match Function
+      this.extra_chars_func),
+    new DirectiveDefinition(["end_of_chorus", "eoc"], false, null, false, ["start_of_chorus", "soc"], false, null, null, null, null,
+      // Directive No Match Function
+      this.extra_chars_func),
+    new DirectiveDefinition(["start_of_tab", "sot"], false, null, true, false, false, null, null, null, null,
+      // Directive No Match Function
+      this.extra_chars_func),
+    new DirectiveDefinition(["end_of_tab", "eot"], false, null, false, ["start_of_tab", "sot"], false, null, null, null, null,
+      // Directive No Match Function
+      this.extra_chars_func),
+    new DirectiveDefinition(["define", ], /([A-G]\w?}) ([Xx0-9]{6})/, null, true, false, true, null,
+      // Value No Match Function
+      function(input: Directive, definition: DirectiveDefinition, output: ChordProValidatorOutput): boolean {
+        // Make sure code parameter is 6 characters long and only contains valid characters
+        if((<RegExp>definition.value).test(input.value) == false) output.warnings.push("Warning: “code” parameter for “define” directive is not exactly 6 characters long or contains characters other than x, X, 0-9. Chord definition is ignored.");
+
+        // Not strictly checking, making a dead assumption that if the code parameter failed, then the name parameter was shot.
+        else output.warnings.push("Warning: “name” parameter for “define” directive does not start with uppercase letters A-G or contains spaces. Chord definition is ignored.");
+
+        // Suppress error.
+        return true;
+      })
+  ];
 
   constructor() { }
 
-  extractDirective(input: string): Object {
-    // Get start and end of directive
-    let start = input.indexOf('{');
-    let end = input.indexOf('}');
-
-    if (start === 0 && end === input.length - 1) {  // Valid directive brackets found
-      // Get separator and value
-      let val: string = null;
-      let separator = input.indexOf(':');   // TODO: replace with .split()?
-      if (separator === -1) {
-        separator = end - 1;
-      }
-      else {
-        // Get value
-        val = input.substr(separator + 1, end - 1 - separator).trim();
-      }
-
-      // Create parsed object and return
-      return {
-        'contents': input.substr(start, end - start + 1).trim(),
-        'directive': input.substr(start + 1, separator - start + 1).trim(),
-        'value': val
-      };
-    }
-    else if (start === -1 && end === -1) {          // No directive found
-      return null;
-    }
-    else {                                        // Malformed directive brackets
-
-      // If bracket missing
-      if(start === -1 || end === -1) {
-        throw "Error: Missing bracket.";
-      }
-      // If start in wrong place
-      else if (start !== 0) {
-        throw "Error: Starting curly brace anywhere other than at the start of the line.";
-      }
-      // If end is in wrong place
-      else if (end !== input.length - 1) {
-        throw "Error: Ending curly brace anywhere other than at the end of the line (after removing whitespace).";
-      }
-      else {
-        throw "Error: Undefined error.";
-      }
-    }
-  }
-
-  // TODO: Create concrete return type
-  validate(input: string): any {
+  /** Validates the input chordpro data and returns a list of errors and warnings.
+   *
+   * @param input
+   * @returns {ChordProValidatorOutput}
+   */
+  validate(input: string): ChordProValidatorOutput {
     // Result arrays
-    let errors: string[] = [];
-    let warnings: string[] = [];
-    let errorsAndWarnings: any = {errors: [], warnings: []};
+    let output: ChordProValidatorOutput = new ChordProValidatorOutput();
 
     // Input data
-    let lines: string[] = input.split(/\r?\n/);
+    let lines: string[] = input.split(/\r?\n/).map(line => line.trim());
 
     // Controls
-    let acceptableDirectives: any =
-      [
-        {'directives': ["new_song","ns"], 'value': false, 'push': true, 'pop': ["new_song", "ns"]},
-        {'directives': ["title",  "t"], 'value': /.*/, 'push': true, 'pop': false, 'stackFunc':function(stack: any[]) {
-          for(let item of stack){
-            if(["title",  "t"].indexOf(item.directive) != -1) {
-              errors.push("Error: More than one “title” directive within the same “new_song” block.");
-              return true;
-            }
-          }
-          return false;}},
-        {'directives': ["subtitle", "st"], 'value': /.*/, 'push': false, 'pop': false},
-        {'directives': ["comment", "c"], 'value': /.*/, 'push': false, 'pop': false},
-        {'directives': ["start_of_chorus", "soc"], 'value': false, 'push': true, 'pop': false},
-        {'directives': ["end_of_chorus", "eoc"], 'value': false, 'push': false, 'pop': ["start_of_chorus", "soc"]},
-        {'directives': ["start_of_tab", "sot"], 'value': false, 'push': true, 'pop': false},
-        {'directives': ["end_of_tab", "eot"], 'value': false, 'push': false, 'pop': ["end_of_tab", "eot"]},
-        {'directives': ["define", ], 'value': /([A-G]\w{0,1}) ([Xx0-9]{6})/, 'push': true, 'pop': false, 'warning': true,
-          'errFunc': function(input: string) {
-            if(/([A-G]\w{0,1})/.test(input) == false) {
-              warnings.push("Warning: “name” parameter for “define” directive does not start with uppercase letters A-G or contains spaces. Chord definition is ignored.");
-            }
-            // Not strictly checking, making a dead assumption
-            else {
-              warnings.push("Warning: “code” parameter for “define” directive is not exactly 6 characters long or contains characters other than x, X, 0-9. Chord definition is ignored.");
-            }
-          }}
-      ];
     let parseStack: any[] = [];
 
-    let lineNum: number = 0;
+    for(let line of lines) {
+      // Ignore comments and blank lines
+      if (line.length <= 0 || line[0] == '#') continue;
 
-    // Error: Missing “title” directive before start of lyrics.
-    //   Error: More than one “title” directive within the same “new_song” block.
-    //   Error: Missing separator (“:”) or text for “title”, “subtitle”, “comment” or “define” directives.
-    //   Error: Extra characters for “soc”, “eoc”, “sot” or “eot” directives.
-    //   Warning: “name” parameter for “define” directive does not start with uppercase letters A-G or contains spaces. Chord definition is ignored.
-    //   Warning: “code” parameter for “define” directive is not exactly 6 characters long or contains characters other than x, X, 0-9. Chord definition is ignored.
-
-    line:
-      for(let line of lines) {
-        // Ignore comments and blank lines
-        if(line.length <= 0 || line[0] == '#') {
-          continue;
-        }
-
-        // Get directive
-        let result: any ;
-        try {
-          result = this.extractDirective(line);
-        } catch(err) {  // Catch any parse errors and move to next line
-          errors.push(err);
-          continue;
-        }
-
-        // If directive, parse
-        if(result !== null) {
-          // Find directive in use
-          for (let directive of acceptableDirectives) {
-
-            // Find specific directive permutation
-            for (let perm of directive.directives) {
-
-              // If directive is found
-              if (result.directive.indexOf(perm) !== -1) {
-
-                // Check if directive has extra characters
-                if (perm.length != result.directive.length) {
-                  errors.push("Error: Extra characters for “soc”, “eoc”, “sot” or “eot” directives.");
-                  continue line; // Leave the nested for loops
-                }
-
-                break;  // Leave permutation checking
-
-              }
-
-            }
-
-            // Check that value is valid
-            console.log(result);
-            console.log(directive);
-            // Value is null but required value isn't
-            if(result.value == null && directive.value != false) {   // NOTE: == is intentional, as it will match empty text or null
-              errors.push("Error: Missing separator (“:”) or text for “title”, “subtitle”, “comment” or “define” directives.");
-            }
-            // Value isn't null but required value is
-            else if (result.value != null && directive.value == false) {
-              errors.push("Error: An undefined value error!");
-            }
-            // Value and required are both false
-            else if (result.value == null && directive.value == false) {
-              // Skip over
-            }
-            // Value and required are both available
-            else if (directive.value.test(result) == false) {
-              if(directive.warning == true) {
-                directive.errFunc(result.value);
-              } else {
-                errors.push("Error: Undefined value error!");
-              }
-            }
-
-            // Value and directives are valid, execute stack commands
-            if (directive.pop != [] && parseStack.length > 0) {   // NOTE: != is intentional, looking for false, empty lists, etc.
-              // While the top of the stack does not contain the directive
-              while (directive.directives.indexOf(parseStack[parseStack.length - 1].directive) !== -1) {
-                // TODO: Handle if we emptied out the whole stack better!
-                parseStack.pop();
-                if (parseStack.length <= 0) break;
-              }
-              parseStack.pop(); // clear off the one to be popped, or last of the stack
-            }
-
-            if (directive.push === true) {
-              parseStack.push(result);
-            }
-
-            // Run custom functions
-            if (directive.stackFunc != undefined) {
-              console.log(directive);
-              if (directive.stackFunc(parseStack) == true) {
-                // Error occurred, although nothing to do so far.
-              }
-            }
-
-            continue line;  // Finished with directive, go to next line TODO: deal with display
-          }
-
-          warnings.push("Warning: Any directive other than one of the supported directives (ignore)");
-        }
-
-        // If lyrics
-        else {
-          // Look for title in stack
-          let title_found = false;
-          for(let directive of parseStack) {
-            if(["title",  "t"].indexOf(directive.directive) != -1) {
-              // title found
-              title_found = true;
-            }
-          }
-
-          // Throw error if no title
-          if(!title_found) {
-            errors.push("Error: Missing “title” directive before start of lyrics.");
-          }
-
-          // TODO: Render for display
-
-          continue line;  // Finished with lyric line, go to next line
-        }
-
+      // Get directive
+      let candidate: Directive;
+      try {
+        candidate = Directive.extractDirective(line);
+      } catch (err) {  // Catch any parse errors and move to next line
+        output.errors.push(err.message);
+        continue;
       }
 
-    // Compile results
-    errorsAndWarnings.errors = errors;
-    errorsAndWarnings.warnings = warnings;
+      // If no directive, continue to next line
+      if (candidate) {
+
+        // Find directive in use
+        let directiveDef = null;
+        for (let directiveDefLoop of this.acceptableDirectives) {
+          if (directiveDefLoop.directiveMatch(candidate, output)) {
+            directiveDef = directiveDefLoop;
+            break;
+          }
+        }
+
+        // If no match found, warn and continue.
+        if (!directiveDef) {
+          output.warnings.push("Warning: Found directive other than one of the supported directives (ignore). Directive: " + candidate.contents);
+          continue;
+        }
+
+        // Check that value is valid
+        if (!directiveDef.valueMatch(candidate, output, true)) continue;
+
+        // Execute stack pops if requested, there is still stack left, and the looked-for directive has not been found yet.
+        while (directiveDef.pop && parseStack.length > 1 && directiveDef.pop.indexOf(parseStack[parseStack.length - 1].directive) == -1) {
+          console.log(parseStack.map(directive => directive.contents));
+          parseStack.pop();
+        }
+
+        // Last one should be popped off; either is the match, or is the last one of the stack
+        // and was going to get cleared regardless.
+        if (parseStack.length && directiveDef.pop && directiveDef.pop.indexOf(parseStack[parseStack.length - 1].directive) == -1) {
+          output.errors.push("Error: Parsing stack in invalid state, could not find pop target. Parsing " + candidate.contents + " Aborting.");
+          break;
+        }
+        if (directiveDef.pop && parseStack.length) parseStack.pop();
+
+        // Execute stack pushes if requested
+        if (directiveDef.push === true) parseStack.push(candidate);
+
+        // Execute stack functions if requested
+        if (!isNullOrUndefined(directiveDef.stackFunc)) {
+          try {
+            directiveDef.stackFunc(parseStack, output)
+          } catch (err) {
+            // Error occurred.
+            console.log(err);
+          }
+        }
+        // Finished with directive, go to next line.
+      }
+
+      // If lyrics
+      else {
+        // Look for title in stack
+        let title_found = false;
+        for(let directive of parseStack) {
+          if(["title",  "t"].indexOf(directive.directive) != -1) {
+            // title found
+            title_found = true;
+            break;
+          }
+        }
+
+        // Throw error if no title
+        if(!title_found) {
+          output.errors.push("Error: Missing “title” directive before start of lyrics.");
+        }
+        // Finished with lyric line, go to next line
+      }
+    }
 
     // Return results
-    return errorsAndWarnings;
+    return output;
   }
 
 }
