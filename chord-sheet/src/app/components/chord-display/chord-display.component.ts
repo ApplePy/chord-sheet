7bdcba6a613842e0f3645ff1afab772ca2c63d36
Darryl Murray <darrylmurray@me.com>
2016-12-02 01:22:01 -0500

    Added chord display, and hooked it up to view-chordsheet and edit-screen (with live update on edit-screen, too)

--- chord-sheet/src/app/components/chord-display/chord-display.component.ts
+++ chord-sheet/src/app/components/chord-display/chord-display.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
+const chordpro = require('chordprojs');
 
 @Component({
   selector: 'app-chord-display',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['chord-display.component.css']
 })
 export class ChordDisplayComponent implements OnInit {
+
+  @Input() set content(inStr: string) {
+    console.log('test');
+    this.renderedContent = chordpro.format(inStr).html;
+  }
+
+  // The rendered content
+  private renderedContent: string = "";
 
   constructor() { }
 
