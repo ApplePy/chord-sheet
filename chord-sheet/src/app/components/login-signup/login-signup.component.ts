80c9751040460d1b055913d2fb74813f629c702a
Darryl Murray <darrylmurray@me.com>
2016-12-04 14:02:02 -0500

    Added CWA button display

--- chord-sheet/src/app/components/login-signup/login-signup.component.ts
+++ chord-sheet/src/app/components/login-signup/login-signup.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginSignupComponent implements OnInit {
 
+  private display: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  showSignUp() {
+    this.display = true;
   }
 
 }
